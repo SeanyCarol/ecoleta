68be4152f007274d9d33a5d5b1d1963b4f16d06e
SeanyCarol <seany@ice.ufjf.br>
2020-06-05 22:47:11 -0300

    Day 4

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -10,7 +10,9 @@ class ItemsController {
             return {
                 id: item.id,
                 title: item.title,
-                image_url: `http://localhost:3333/uploads/${item.image}`,
+                //para rodar na web: `http://localhost:3333/uploads/${item.image}`
+                //para rodar no mobile: `http://192.168.100.6:3333/uploads/${item.image}`
+                image_url: `http://192.168.100.6:3333/uploads/${item.image}`,
             };
         });
     
