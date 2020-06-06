41d1d9ab1e85d392ad40a1a3bc84ddbe5cf47a7b
SeanyCarol <seany@ice.ufjf.br>
2020-06-06 15:25:13 -0300

    arquivos incompletos

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -21,7 +21,15 @@ class PointsController{
             .distinct()
             .select('points.*');
 
-        return response.json(points);
+        //Transforma os dados pra um outro formato o qual vai ser mais acessícel para quem está requisitando as infos
+        const serializedPoints = points.map( point => {
+            return {
+                ...point,
+                image_url: `http://192.168.100.6:3333/uploads/${point.image}`,
+            };
+        });
+            
+        return response.json(serializedPoints);
     }
 
     async show(request: Request, response: Response) {
@@ -33,12 +41,17 @@ class PointsController{
             return response.status(400).json({message: 'Point not found.'});
         }
 
+        const serializedPoint = {
+            ...point,
+            image_url: `http://192.168.100.6:3333/uploads/${point.image}`,
+        };
+
         const items = await knex('items')
             .join('point_items', 'items.id', '=', 'point_items.item_id')
             .where('point_items.point_id', id)
             .select('items.title');
         
-        return response.json({point, items});
+        return response.json({point: serializedPoint, items});
     }
     //Criação de pontos de coleta
     async create(request: Request, response: Response) {
@@ -53,8 +66,11 @@ class PointsController{
             items
         } = request.body;
 
+        //Para testar se caso a segunda query falhar, a primeira não vai executar
+        const trx = await knex.transaction();
+
         const point = {
-            image: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+            image: request.file.filename,
             name,
             email,
             whatsapp,
@@ -63,19 +79,19 @@ class PointsController{
             city,
             uf
         };
-    
-        //Para testar se caso a segunda query falhar, a primeira não vai executar
-        const trx = await knex.transaction();
-    
+          
         const insertedIds = await trx('points').insert(point);
     
         const point_id = insertedIds[0];
     
-        const pointItems  = items.map((item_id: number) => {
-            return {
-                item_id,
-                point_id,
-            };
+        const pointItems  = items
+            .split(',')
+            .map((item: string) => Number(item.trim()))
+            .map((item_id: number) => {
+                return {
+                    item_id,
+                    point_id,
+                };
         });
     
         await trx('point_items').insert(pointItems);
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
@@ -9,16 +11,19 @@ import ItemsController from './controllers/ItemsController';
 //create, update, delete
 
 const routes = express.Router();
+const upload = multer(multerConfig);
+
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
 //Listar items
 routes.get('/items', itemsController.index);
-//Criação do ponto de coleta
-routes.post('/points', pointsController.create);
 //Listar os pontos
 routes.get('/points', pointsController.index);
 //Listar ponto de coleta específico
 routes.get('/points/:id', pointsController.show);
+
+//Criação do ponto de coleta
+routes.post('/points', upload.single('image'), pointsController.create);
 
 export default routes;
